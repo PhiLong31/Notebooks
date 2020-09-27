611766051986439822ea1ae6e4d5ca71fce1d916
HuongM-Mai <nhmai.18it4@sict.udn.vn>
2020-09-27 11:10:06 +0700

    readdata

---
+++ app/src/main/java/com/example/notebooks/Adapter/AdapterNote.java
@@ -0,0 +1,58 @@
+package com.example.notebooks.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.notebooks.MainActivity;
+import com.example.notebooks.Model.Model;
+import com.example.notebooks.R;
+
+import java.util.List;
+
+public class AdapterNote extends RecyclerView.Adapter<AdapterNote.ViewHolder> {
+
+    private MainActivity context;
+    private List<Model> listnote;
+
+    public AdapterNote(MainActivity context, List<Model> listnote) {
+        this.context = context;
+        this.listnote = listnote;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View objview = LayoutInflater.from(context).inflate(R.layout.note_item, parent, false);
+        return new ViewHolder(objview);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Model model = listnote.get(position);
+        TextView tv_title = holder.title;
+        TextView tv_content = holder.content;
+
+        tv_title.setText(model.getTitle());
+        tv_content.setText(model.getContent());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listnote.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title, content;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.txt_title);
+            content = (TextView) itemView.findViewById(R.id.txt_content);
+        }
+    }
+}
--- app/src/main/java/com/example/notebooks/MainActivity.java
+++ app/src/main/java/com/example/notebooks/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.notebooks;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.notebooks.Adapter.AdapterNote;
+import com.example.notebooks.Model.Model;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private FloatingActionButton fab;
+    private RecyclerView recyclerView;
+    private ArrayList<Model> arrayList;
+    private AdapterNote adapter;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +37,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
         detail();
+        SelectData();
             }
-            private void init() {
+    private void init() {
         fab = (FloatingActionButton) findViewById(R.id.fab);
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        RecyclerView.LayoutManager layoutManager = linearLayoutManager;
+        recyclerView.setLayoutManager(layoutManager);
     }
 
     private void detail() {
@@ -32,6 +57,34 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+    public void SelectData(){
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("note");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                arrayList = new ArrayList<>();
+                arrayList.clear();
+                Model model = new Model();
+                for(DataSnapshot data: dataSnapshot.getChildren()){
+                    model = data.getValue(Model.class);
+                    model.setId(data.getKey());
+                    arrayList.add(model);
+                    Log.d("id","onDataChange: "+model.getId());
+                    Log.d("title","onDataChange: "+model.getTitle());
+                }
+
+                adapter= new AdapterNote(MainActivity.this,arrayList);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
 }
